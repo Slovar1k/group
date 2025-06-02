98fde92107b6edbc44767bb386d888acabbd0be5
Dakemani <artemr0307@gmail.com>
2025-06-02 11:22:24 +0300

    Update Python_group.py
    Ромаович эдит

--- Python_group.py
+++ Python_group.py
@@ -2,3 +2,18 @@ z=int(input)
 x=int(input)
 s=x*z
 print(s)
+
+---------------------------
+ТРЭУГОЛЬНИК 
+
+def is_triangle(a, b, c):
+    if a + b > c and a + c > b and b + c > a:
+        return True
+    return False
+
+------------------------------
+
+
+
+
+
